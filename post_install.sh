6e6aeb343d7d000678c45534662be75066f569bd
Hendrik Borras <borras@stud.uni-heidelberg.de>
2016-10-07 17:06:54 +0200

    Added configuration for keras to use Theano

--- post_install.sh
+++ post_install.sh
@@ -5,3 +5,15 @@ mkdir /var/log/netTel/
 mkdir /var/spool/netTel/input/
 mkdir /var/spool/netTel/output/
 
+# configure the keras backend to use theano
+# maybe this should be done done somewhere else?
+mkdir ~/.keras/
+touch ~/.keras/keras.json
+cat > ~/.keras/keras.json <<EOL
+{
+    "image_dim_ordering": "tf",
+    "epsilon": 1e-07,
+    "floatx": "float32",
+    "backend": "theano"
+}
+EOL
